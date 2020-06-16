9b9c1b97b1052084f6561c160288c642d92246d7
tnarik <tnarik@lecafeautomatique.co.uk>
2020-06-16 08:30:22 +0100

    git-cafe: update 0.0.1 bottle.

--- Formula/git-cafe.rb
+++ Formula/git-cafe.rb
@@ -6,6 +6,12 @@ class GitCafe < Formula
     :revision => "a0cab7608c5805fd2f55d25477a54083e28eeb71"
   version "0.0.1"
 
+  bottle do
+    root_url "https://dl.bintray.com/tnarik/bottles-tap"
+    cellar :any_skip_relocation
+    sha256 "56054df145fa307eb5170ad5b2fbe9def6ef53615fea95f43202079cc051736a" => :catalina
+  end
+
   # Could add depends_on "git", but let's not require the installation as git-cafe is more of a plugin
 
   def install
