fee1715f529e7c1d67e704279ae280249fc07950
tnarik <tnarik@lecafeautomatique.co.uk>
2020-07-05 12:47:02 +0100

    Temptatively updated formula which includes git-bkup and the python3 dependency.

--- Formula/git-cafe.rb
+++ Formula/git-cafe.rb
@@ -3,8 +3,8 @@ class GitCafe < Formula
   homepage "https://gogs.lecafeautomatique.net/tnarik/git_custom"
   url "git@gitcafe:tnarik/git_custom.git",
     :using    => :git,
-    :revision => "419e001af9"
-  version "0.0.2"
+    :revision => "6c14c4e6b437c64940eb350f4d5093b6db742cbd"
+  version "0.0.3"
 
   bottle do
     root_url "https://dl.bintray.com/tnarik/bottles-brew"
@@ -13,16 +13,19 @@ class GitCafe < Formula
   end
 
   # Could add depends_on "git", but let's not require the installation as git-cafe is more of a plugin
+  depends_on "python3"
 
   def install
     bin.install "git-all" => "git-all"
     bin.install "git-create" => "git-create"
     bin.install "git-cafelist" => "git-cafelist"
+    bin.install "git-bkup" => "git-bkup"
   end
 
   test do
     assert_match "info, do it right", shell_output("#{bin}/git-all", 127)
     assert_match /Creates a repository.*/, shell_output("#{bin}/git-create", 127)
     assert_match /List all repos.*/, shell_output("#{bin}/git-cafelist -h", 127)
+    assert_match /usage: git-bkup .*/, shell_output("#{bin}/git-bkup -h", 127)
   end
 end
