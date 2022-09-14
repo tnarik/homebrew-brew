415042921d5e115d7e7f344a8cf9f80d376e1c4c
tnarik <tnarik@lecafeautomatique.co.uk>
2022-09-14 22:10:42 +0100

    git-cafe: update 0.0.3 bottle.

--- Formula/git-cafe.rb
+++ Formula/git-cafe.rb
@@ -6,6 +6,12 @@ class GitCafe < Formula
     revision: "a0bbe10c2721f97737c7106daa57c652903e0a1b"
   version "0.0.3"
 
+  bottle do
+    root_url "https://github.com/tnarik/homebrew-brew/releases/download/git-cafe-0.0.3"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, catalina: "6f027d6264591432f76d8b25cd8c235688f037b10b15b4deda9de7633544668f"
+  end
+
   # Could add depends_on "git", but let's not require the installation as git-cafe is more of a plugin
   depends_on "python"
 
